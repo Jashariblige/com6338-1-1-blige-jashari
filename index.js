e9b6d107a01c8f3e94800e2fc97a528b60a27846
jashari <jashariblige@gmail.com>
2023-01-22 19:21:56 -0600

    input js code

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Your code here
+
+function greet() {
+
+    var name  = prompt('What is your name?');
+    alert('Hello, ' + name); 
+ 
+    var age = parseInt(prompt('How old are you?'));
+    var confirmBday = confirm('Did you have a birthday this year? If Yes, hit okay. If not, hit cancel.');
+    var currentYear = new Date().getFullYear();
+    
+    if (confirmBday) {
+        birthYear = currentYear - age;
+    }
+    else {
+        birthYear = currentYear - age - 1;
+    }
+
+    alert('You were born in: ' + birthYear);
+
+}
